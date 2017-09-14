05bf2ee3a57bc88b111aaf0fa8e576d514107a07
Rana Bhattacharyya <ranab33@gmail.com>
2017-09-14 13:04:27 -0400

    add app def sign in and session route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def sign_in(user)
+    token = SecureRandom.urlsafe_base64
+    session[:session_token] = token
+    user.update_attribute(:session_token, token)
+  end 
 end
