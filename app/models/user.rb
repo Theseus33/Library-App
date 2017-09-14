8382a55263f28d3993564bb9328c4c653e10de08
Rana Bhattacharyya <ranab33@gmail.com>
2017-09-14 11:17:08 -0400

    add user model auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,26 @@
 class User < ApplicationRecord
+    PASSWORD_LENGTH = (5..20)
+    USERNAME_LENGTH = (5..20)
+
+    validates_presence_of :username
+    validates :username, length: USERNAME_LENGTH, uniqueness: true
+    validates :password, length: PASSWORD_LENGTH, allow_nil: true
+
+    has_many :books
+
+    def self.find_from_credentials(username, password)
+        user = find_by(username: username)
+        return nil unless user
+        user if user.is_password?(password)
+    end
+
+    def is_password?(password_attempt)
+        BCrypt::Password.new(password_digest).is_password?(password_attempt)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+    
 end
--- db/migrate/20170914135915_create_users.rb
+++ db/migrate/20170914135915_create_users.rb
@@ -8,6 +8,6 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.timestamps
     end
     add_index :users, :username
-    add_index :users, :sessiontoken
+    add_index :users, :session_token
   end
 end
