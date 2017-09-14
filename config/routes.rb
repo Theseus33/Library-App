1df43011add3e7e329081acf95e9fa3aee7cb088
Rana Bhattacharyya <ranab33@gmail.com>
2017-09-14 14:51:50 -0400

    add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   root to: 'sessions#new'
 
+  
+  resources :users, only: [:new, :create, :index, :show]
+
   resource :session, only [:new, :create, :destroy]
 
 end
