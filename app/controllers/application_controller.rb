c5843a93248834a7b69c4e2a0b24a8f4aac0a2a3
Rana Bhattacharyya <ranab33@gmail.com>
2017-09-14 14:36:27 -0400

    add sign out to app_controller and define current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  helper_method :current user
 
   def sign_in(user)
     token = SecureRandom.urlsafe_base64
     session[:session_token] = token
     user.update_attribute(:session_token, token)
   end 
+
+  def sign_out(user)
+    return unless current_user
+    current_user.update!(session_token: nil)
+    session.delete(:session_token)
+  end
+
+  def current_user 
+    @current_user || find_current_user
+  end
+
+  def find_current_user
+    session_token = session[:session_token]
+    session_token && User.find_by(session_token: session_token)
+  end
+  
 end
