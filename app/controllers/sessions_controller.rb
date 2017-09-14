35c280a4633f061aeb515b39acc4b85ef05f1ee8
Rana Bhattacharyya <ranab33@gmail.com>
2017-09-14 16:32:40 -0400

    add sessions controller methods and view with placeholder sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,38 @@
 class SessionsController < ApplicationController
+
+    before_action :ensure_signed_out, only: [:new, create]
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        username = user_params[:username]
+        password = user_params[:password]
+
+        user = User.find_from_credentials(username, password)
+
+        if user
+            sign_in(user)
+            flash[:notice] = "#{username}, You are signed in!"
+            redirect_to users_path
+        else
+            flash[:error] = "User not found"
+            @user = User.new(username: username)
+            render :new
+        end
+    end
+
+    def destroy
+        sign_out 
+        flash[:notice] = 'You signed out'
+        redirect_to sessions_path
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
