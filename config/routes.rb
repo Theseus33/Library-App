324014f532000f1d01a648abc7dacdf25ebb40db
Rana Bhattacharyya <ranab33@gmail.com>
2017-09-14 14:53:21 -0400

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   
   resources :users, only: [:new, :create, :index, :show]
 
-  resource :session, only [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
 end
