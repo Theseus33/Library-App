8b623b4f48dd4986bb6cfa99e743c8e009e6f420
Rana Bhattacharyya <ranab33@gmail.com>
2017-09-18 13:53:01 -0400

    add minor views changes

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ApplicationRecord
     belongs_to :user
     belongs_to :category
 
-    has_attached_file :avatar, styles: { :book_index => "250x375>", :book_show => "300x475>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :book_img, styles: { :book_index => "250x375>", :book_show => "350x500>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
 end
