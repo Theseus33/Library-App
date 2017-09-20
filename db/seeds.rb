159b22369a205fa207e5606ec893cd075dd40b09
Rana Bhattacharyya <ranab33@gmail.com>
2017-09-20 04:47:02 -0400

    added seed data for genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create!([
+    {
+      name: "Arts & Photography",
+    },
+    {
+      name: "Biographies & Memoirs",
+    },
+    {
+      name: "Business & Investing",
+    },
+    {
+      name: "Children's Books",
+    },
+    {
+      name: "Comics & Graphic Novels",
+    },
+    {
+      name: "Cookbooks",
+    },
+    {
+      name: "Crafts & Hobbies",
+    },
+    {
+      name: "Computers & Technology",
+    },
+        {
+      name: "Education & Referance",
+    },
+    {
+      name: "Fantasy",
+    },
+    {
+      name: "Fiction",
+    },
+    {
+      name: "Health & Fitness",
+    },
+    {
+      name: "History",
+    },
+    {
+      name: "Humor & Entertainment",
+    },
+    {
+      name: "Law",
+    },
+    {
+      name: "Medicine",
+    },
+    {
+      name: "Mystery & Suspense",
+    },
+    {
+      name: "Parenting",
+    },
+    {
+      name: "Politics & Social Sciences",
+    },
+    {
+      name: "Religion & Spirituality",
+    },
+        {
+      name: "Romance",
+    },
+    {
+      name: "Science & Math",
+    },
+    {
+      name: "Science Fiction",
+    },
+    {
+      name: "Sports",
+    }
+    ])
